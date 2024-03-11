dd6f35d0351110e87c4997e0c81458451d00f8b9
Jane J. Yosef <stupidjane@tutanota.com>
2024-03-11 18:17:55 +0100

    Add German Localization

---
+++ i18n/de_de.py
@@ -0,0 +1,127 @@
+LANGUAGE = "de_de"
+LANGUAGE_NAME = "Deutsch (DE)"
+
+class Strings:
+    # Onboarding Strings
+    onboarding_title = "NeXTPlan - Onboarding"
+    onboarding_subtitle = "Willkommen bei NeXTPlan! Lass uns beginnen"
+    onboarding_pre_text = "Dieser Onboarding-Prozess wird Sie durch die Einrichtung Ihres NeXTPlan-Kontos führen. Sie werden gebeten, Anmeldeinformationen sowie einige grundlegende Informationen über Sie und Ihre Schule bereitzustellen."
+    
+    onboarding_step0_title = "Ist dies die richtige Sprache?"
+    onboarding_step0_text = "Diese App ist in mehreren Sprachen verfügbar. Wenn Sie die Sprache ändern möchten, tun Sie dies jetzt. Andernfalls klicken Sie auf 'Weiter', um fortzufahren."
+
+    
+    onboarding_step1_title = "Bitte geben Sie zuerst Ihre Schulnummer ein"
+    onboarding_step1_text = "Es handelt sich um eine 8-stellige Nummer, die Ihnen von Ihrer Schule zur Verfügung gestellt wurde. Wenn Sie sie nicht haben, wenden Sie sich bitte an Ihren Schuladministrator."
+    onboarding_step1_hint = "Schulnummer"
+    onboarding_step2_title = "Bitte geben Sie Ihre Anmeldeinformationen ein."
+    onboarding_step2_text = "Geben Sie Ihren Benutzernamen (höchstwahrscheinlich 'schueler') und Ihr Passwort ein. Wenn Sie diese nicht haben, wenden Sie sich bitte an Ihren Schuladministrator."
+    onboarding_step2_hint_username = "Benutzername"
+    onboarding_step2_hint_password = "Passwort"
+
+    onboarding_step3_title = "Geben Sie jetzt noch ihre persönlichen Informationen ein"
+    onboarding_step3_text = "Diese Informationen werden verwendet, um Ihre Erfahrung anzupassen und Ihnen den bestmöglichen Service zu bieten."
+    onboarding_step3_hint_firstname = "Vorname"
+    onboarding_step3_hint_lastname = "Nachname"
+    onboarding_step3_hint_pronouns = "Pronomen (optional, z.B. xier/xiem. Dies wird verwendet, um Sie in der App anzusprechen.)"
+
+    onboarding_step4_title = "Wählen Sie Ihre Klasse/Ihren Kurs aus."
+    onboarding_step4_text = "Wählen Sie die Klasse oder den Kurs aus, den Sie derzeit besuchen. Wenn Sie sich nicht sicher sind, wenden Sie sich bitte an Ihren Schuladministrator."
+
+    onboarding_step5_title = "Sie sind fertig!"
+    onboarding_step5_text = "Sie haben den Onboarding-Prozess erfolgreich abgeschlossen. Klicken Sie auf 'Senden', um Ihre Informationen zu speichern und mit der App fortzufahren."
+
+    # UI Strings
+
+    next_button = "Weiter"
+    back_button = "Zurück"
+    submit_button = "Senden"
+    cancel_button = "Abbruch"
+
+    # Error Strings
+    error_title = "Fehler"
+    error_text = "Ein Fehler ist aufgetreten. Bitte versuchen Sie es erneut."
+
+    # Info Strings
+    progress_title = "Einen Moment..."
+    date_format = "%d.%m.%Y"
+
+    # Home Screen Strings
+    home_wm_title = "NeXTPlan - Home"
+    home_nav = "Home"
+    home_title = "Hallo, %name!"
+
+    # Settings Screen Strings
+    settings_nav = "Einstellungen"
+    settings_wm_title = "NeXTPlan - Einstellungen"
+    settings_title = "Einstellungen"
+    settings_language_title = "Sprache"
+    settings_language_text = "Wählen Sie Ihre bevorzugte Sprache."
+
+    settings_credentials_title = "Zugangsdaten"
+    settings_credentials_text = "Ändern Sie Ihre Anmeldeinformationen."
+    settings_credentials_username = "Benutzername"
+    settings_credentials_password = "Passwort"
+    settings_credentials_schoolid = "Schulnummer"
+
+    settings_theme_title = "Farbschema"
+
+    settings_personal_title = "Persönliche Informationen"
+    settings_personal_text = "Ändern Sie Ihre persönlichen Informationen."
+    settings_personal_firstname = "Vorname"
+    settings_personal_lastname = "Nachname"
+    settings_personal_pronouns = "Präferierte Pronomen"
+    settings_personal_firstname_hint = "Vorname"
+    settings_personal_lastname_hint = "Nachname"
+    settings_personal_pronouns_hint = "Pronomen (optional, z.B. xier/xiem. Dies wird verwendet, um Sie in der App anzusprechen.)"
+
+    settings_class_title = "Klasse/Kurs"
+    settings_class_text = "Wählen Sie Ihre Klasse/Ihren Kurs aus."
+
+    settings_enable_grades_title = "Noten aktivieren (Experimentell)"
+    settings_enable_grades_text = "Aktivieren Sie die Anzeige Ihrer Noten. Dies ist eine experimentelle Funktion und kann unerwartetes Verhalten verursachen."
+    settings_grades_pin_title = "Noten-PIN"
+    settings_grades_pin_text = "Erstellen Sie eine PIN, um Ihre Noten zu schützen."
+    settings_grades_pin_subtext = "Die PIN wird verwendet, um Ihre Noten zu schützen. Sie wird benötigt, um Ihre Noten anzuzeigen oder zu ändern."
+    settings_grades_pin_hint = "PIN"
+    settings_grades_pin_confirm_hint = "PIN bestätigen"
+    settings_grades_pin_save_button = "PIN speichern"
+
+    settings_advanced_title = "Erweitert"
+    settings_advanced_text = "Erweiterte Einstellungen anzeigen."
+
+    settings_advanced_clear_cache_and_data_title = "Cache und Daten löschen"
+
+    settings_advanced_clear_cache_button = "Cache löschen"
+    settings_advanced_clear_cache_text = "Löschen Sie den Cache der App. Dies kann dazu führen, dass die App langsamer wird, aber es kann auch Probleme beheben."
+
+    settings_advanced_clear_data_button = "Daten löschen"
+    settings_advanced_clear_data_text = "Löschen Sie alle Daten der App. Dies wird Sie ausloggen und alle Daten (einschließlich Noten) von Ihrem Gerät entfernen."
+    settings_advanced_clear_data_confirm_button = "Daten löschen"
+    settings_advanced_clear_data_confirm_prompt = "Sind Sie sicher, dass Sie alle Daten löschen möchten? Diese Aktion kann nicht rückgängig gemacht werden."
+
+    settings_advanced_logs_title = "Logs"
+    settings_advanced_logs_text = "Zeigen Sie die App-Logs an."
+
+    settings_source_code_title = "Quellcode & Lizenzen"
+    settings_main_code_text = "App-Quellcode"
+    settings_main_code_subtext = "Der Quellcode dieser App ist auf GitHub verfügbar. Sie können ihn anzeigen, dazu beitragen und für Ihre eigenen Projekte verwenden."
+    settings_i18n_code_text = "Internationalisierungs-Quellcode"
+    settings_i18n_code_subtext = "Der Quellcode für die Internationalisierung dieser App ist auf GitHub verfügbar. Sie können ihn anzeigen, dazu beitragen und für Ihre eigenen Projekte verwenden."
+    settings_licenses_text = "Lizenzen"
+    settings_licenses_subtext = "Zeigen Sie die Lizenzen der in dieser App verwendeten Software an."
+    settings_view_license = "Lizenz anzeigen"
+
+
+    settings_save_button = "Speichern"
+    settings_save_success = "Ihre Einstellungen wurden gespeichert."
+
+    ## VPlan
+    vplan_nav = "VPlan"
+    vplan_wm_title = "NeXTPlan - Vertretungsplan"
+    vplan_title = "Vertretungsplan"
+    vplan_loading = "Laden..."
+    vplan_no_data = "Keine Daten verfügbar."
+
+    vplan_free = "Kein Unterricht"
+    vplan_no_teacher = "Kein Lehrer - Aufgaben erteilt"
