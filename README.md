4147a4e30b9625fa579d9e7fb8781ce44878bced
Jane J. Yosef <stupidjane@tutanota.com>
2024-03-11 15:54:42 +0100

    Initial (Basic) Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Internationalization for the NeXTPlan App
+
+## Introduction
+The NeXTPlan App supports multiple languages through internationalization. This allows users to view the app's content in their preferred language.
+
+## What is Internationalization?
+Internationalization is the process of designing and developing software so that it can be easily translated into different languages and regions. This is done by separating the text and other content from the source code and storing it in separate files. These files are then translated into the desired languages.
+
+## Contributing
+If you would like to contribute translations for additional languages or improve existing translations, please follow these guidelines:
+
+1. Fork the NeXTPlan repository.
+2. Create a new branch for your translations.
+3. Make the necessary changes to the language files.
+4. Submit a pull request with your changes.
+
+## License
+The NeXTPlan App is released under the MIT License. See [LICENSE](./LICENSE) for more information.
---
+++ i18n/en_us.py
@@ -0,0 +1,40 @@
+LANGUAGE = "en_us"
+LANGUAGE_NAME = "English (US)"
+
+class Strings:
+    # Onboarding Strings
+    onboarding_title = "NeXTPlan - Onboarding"
+    onboarding_subtitle = "Welcome to NeXTPlan! Let's get you started."
+    onboarding_pre_text = "This onboarding process will guide you through the setup of your NeXTPlan account. You will be asked to provide credentials as well as some basic information about you and your school."
+    
+    onboarding_step0_title = "Is this the correct Language?"
+    onboarding_step0_text = "This app is available in multiple languages. If you would like to change the language, please do so now. Otherwise, click 'Next' to continue."
+
+    
+    onboarding_step1_title = "Please enter your School ID Number first."
+    onboarding_step1_text = "It's an 8-digit number that has been provided to you by your School. If you don't have it, please contact your School Administrator."
+    onboarding_step1_hint = "School ID"
+    onboarding_step2_title = "Please enter your Login Credentials."
+    onboarding_step2_text = "Enter your username (Most likely 'schueler') and password. If you don't have these, please contact your School Administrator."
+    onboarding_step2_hint_username = "Username"
+    onboarding_step2_hint_password = "Password"
+
+    onboarding_step3_title = "Finally, please enter your personal information."
+    onboarding_step3_text = "This information will be used to customize your experience and to provide you with the best possible service."
+    onboarding_step3_hint_firstname = "First Name"
+    onboarding_step3_hint_lastname = "Last Name"
+    onboarding_step3_hint_pronouns = "Pronouns (optional, e.g. they/them. This will be used to address you in the app.)"
+
+    onboarding_step4_title = "You're all set!"
+    onboarding_step4_text = "You have successfully completed the onboarding process. Click 'Submit' to save your information and continue to the app."
+
+    # UI Strings
+
+    next_button = "Next"
+    back_button = "Back"
+    submit_button = "Submit"
+    cancel_button = "Cancel"
+
+    # Error Strings
+    error_title = "Error"
+    error_text = "An error occurred. Please try again."
