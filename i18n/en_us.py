4b4d9499725541238f7c941db5ce5effc3a4ff5f
Jane J. Yosef <stupidjane@tutanota.com>
2024-03-11 16:35:00 +0100

    Add Settings i18n strings

--- i18n/en_us.py
+++ i18n/en_us.py
@@ -41,7 +41,61 @@ class Strings:
 
     # Info Strings
     progress_title = "Please wait..."
+    date_format = "%d.%m.%Y"
 
     # Home Screen Strings
     home_wm_title = "NeXTPlan - Home"
     home_title = "Welcome, %name!"
+
+    # Settings Screen Strings
+    settings_wm_title = "NeXTPlan - Settings"
+    settings_title = "Settings"
+    settings_language_title = "Language"
+    settings_language_text = "Select your preferred language."
+
+    settings_credentials_title = "Credentials"
+    settings_credentials_text = "Change your login credentials."
+    settings_credentials_username = "Username"
+    settings_credentials_password = "Password"
+    settings_credentials_schoolid = "School ID"
+
+    settings_personal_title = "Personal Information"
+    settings_personal_text = "Change your personal information."
+    settings_personal_firstname = "First Name"
+    settings_personal_lastname = "Last Name"
+    settings_personal_pronouns = "Pronouns"
+    settings_personal_firstname_hint = "First Name"
+    settings_personal_lastname_hint = "Last Name"
+    settings_personal_pronouns_hint = "Pronouns (optional, e.g. they/them. This will be used to address you in the app.)"
+
+    settings_class_title = "Class"
+    settings_class_text = "Change your class/course."
+
+    settings_enable_grades_title = "Enable Grades (Experimental)"
+    settings_enable_grades_text = "Enable the display and storage of your grades in the app. This feature is experimental and may not work as expected."
+    settings_grades_pin_title = "Grades PIN"
+    settings_grades_pin_text = "Your grades are your personal information. To protect your privacy, you can set a PIN that is required to access your grades"
+    settings_grades_pin_subtext = "Your PIN will be stored securely on your device and will not be shared with anyone."
+    settings_grades_pin_hint = "PIN"
+    settings_grades_pin_confirm_hint = "Confirm PIN"
+    settings_grades_pin_save_button = "Save PIN"
+
+    settings_advanced_title = "Advanced"
+    settings_advanced_text = "Advanced settings and features."
+
+    settings_advanced_clear_cache_and_data_title = "Clear Cache and Data"
+
+    settings_advanced_clear_cache_button = "Clear Cache"
+    settings_advanced_clear_cache_text = "Clear the app's cache. This will remove all temporary files and data stored on your device."
+
+    settings_advanced_clear_data_button = "Clear Data"
+    settings_advanced_clear_data_text = "Clear the app's data. This will log you out and remove all data stored on your device. This action cannot be undone."
+    settings_advanced_clear_data_confirm_button = "Clear Data"
+    settings_advanced_clear_data_confirm_prompt = "Are you sure you want to clear all data? This action cannot be undone. You will be logged out and all data (including grades) will be removed from your device."
+
+    settings_advanced_logs_title = "Logs"
+    settings_advanced_logs_text = "View and send logs to the developer for troubleshooting."
+
+    settings_save_button = "Save"
+    settings_save_success = "Your settings have been saved."
+
